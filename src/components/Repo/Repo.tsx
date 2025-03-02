3bb5ad4549e606c537ebb867f721c3de40ef6ee2
mohammadfaramarzi1 <faramarzi.dev@gamil.com>
2025-03-02 17:51:12 +0330

    feat: Repositories

--- src/components/Repo/Repo.tsx
+++ src/components/Repo/Repo.tsx
@@ -1,7 +1,46 @@
-function Repo() {
+import { Repo as RepoType } from "../../types/repo.types";
+import { FaRegStar } from "react-icons/fa";
+import { MdDoubleArrow } from "react-icons/md";
+
+import styles from "./Repo.module.css";
+import { Link } from "react-router-dom";
+
+function Repo({
+  description,
+  html_url,
+  name,
+  language,
+  stargazers_count,
+}: RepoType) {
   return (
-    <div>Repo</div>
-  )
+    <div className={styles.wrapper}>
+      <div className={styles.repo}>
+        <div className={styles.info}>
+          <p>
+            Repo: <span>{name}</span>
+          </p>
+          <p>
+            Language: <span>{language}</span>
+          </p>
+        </div>
+        <div className={styles.desc}>
+          <p>
+            Description: <span>{description}</span>
+          </p>
+          <p className={styles.stars}>
+            Stars:
+            <span>
+              {stargazers_count}<FaRegStar />
+            </span>
+          </p>
+        </div>
+        <Link to={`${html_url}`} className={styles.link}>
+          See Details
+          <MdDoubleArrow />
+        </Link>
+      </div>
+    </div>
+  );
 }
 
-export default Repo
+export default Repo;
--- src/components/Repositories/Repositories.tsx
+++ src/components/Repositories/Repositories.tsx
@@ -1,8 +1,10 @@
 import { RootState } from "@reduxjs/toolkit/query";
 import { useSelector } from "react-redux";
 import Loader from "../Loader/Loader";
-import Error from "../Error/Error";
 import Repo from "../Repo/Repo";
+import { Repo as RepoType } from "../../types/repo.types";
+
+import styles from "./Repositories.module.css"
 
 function Repositories() {
   const { error, loading, repos } = useSelector(
@@ -13,10 +15,11 @@ function Repositories() {
   return (
     <div>
       {loading && <Loader />}
-      {error && <Error />}
-      {/* {
-        repos && repos.map(repo => <Repo key={repo.id} />)
-      } */}
+      {error && <p className={styles.error}>Something went wrong!</p>}
+      <div className={styles.repositories}>
+        {repos &&
+          repos.map((repo: RepoType) => <Repo key={repo.id} {...repo} />)}
+      </div>
     </div>
   );
 }
